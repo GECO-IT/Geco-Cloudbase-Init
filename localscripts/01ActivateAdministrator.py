86628469a290d03ac404e92c4b90c2c6ed6ead0d
c.soylu <csoylu@debian-BULLSEYE-live-builder-AMD64>
2022-01-18 13:13:57 +0100

    Script compatible all languages, ModifyConf ps1

--- localscripts/01ActivateAdministrator.py
+++ localscripts/01ActivateAdministrator.py
@@ -1,4 +1,4 @@
-import os,json,sys,subprocess
+import os,json,sys,subprocess,configparser,platform
 
 def find_drive(file_path):
     for number in range(65,91):
@@ -32,17 +32,38 @@ def enable_administrator_account():
     print("\n Administrator account is activated by localscript")
     return run.stdout
 
+def is_os_64bit():
+    return platform.machine().endswith('64')
+
+def get_data(variable,path):
+    configParser = configparser.RawConfigParser()
+    configParser.read(path)
+    data = configParser.get('DEFAULT',variable)
+    return data
+
+
 # variables
 meta_data_path = find_drive(":\OPENSTACK\LATEST\META_DATA.json")
 admin_name = get_administrator_name()
 
 # execute
-if meta_data_path != "False":
+if (meta_data_path) and ("admin_username" in load_json_file(meta_data_path,"meta")):
     meta_data = load_json_file(meta_data_path,"meta")
+    meta_username = meta_data["admin_username"]
+    print("Meta_Data admin_username is :" + meta_username)
 else:
-    sys.exit(0)
+    if is_os_64bit():
+        conf_path = r'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf'
+        print("System architecture is 64 bit.")
 
-if meta_data["admin_username"] in admin_name and "False" in get_administrator_status():
+    else:
+        conf_path = r'C:\Program Files (x86)\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf'
+        print("System architecture is 32 bit.")
+
+    meta_username = get_data('username', conf_path)
+    print("Conf username:"+meta_username)
+
+if meta_username in admin_name and "False" in get_administrator_status():
     run = enable_administrator_account()
     sys.exit(1001)
 else:
